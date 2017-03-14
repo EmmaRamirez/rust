e4d73afe1b840c81603bb3f2f87b5fd5deffcc2f
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-14 00:56:59 -0600

    add more examples for closures

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -26,7 +26,37 @@ fn main() {
     {
         let plus_num = |x: i32| x + num;
     }
-
     let y = &mut num;
+
+    let mut num = 5;
+
+    {
+        // Takes ownership
+        let mut add_num = move |x: i32| num += x;
+        add_num(5);
+    }
+
+    fn call_with_one<F>(some_closure: F) -> i32
+        // Because Fn is a trait, we can use it as a bound for our
+        // generic type. In this case, our closure takes an i32
+        // as an argument and returns i32, so the generic bound
+        // we use Fn(i32) -> i32
+        // This code gets monomorphized, and therefore, we'll
+        // be doing static dipsatch into the closure.
+        // In many languages, closures are inherently heap
+        // allocated, and will always involve dynamic dispatch
+        // In Rust, we can stack allocate our closure environment
+        // and statically dispatch the call.
+        where F: Fn(i32) -> i32 {
+            some_closure(1)
+    }
+
+    let answer = call_with_one(|x| x + 2);
+
+    fn call_with_one_2(some_closure: &Fn(i32)) -> i32 {
+        some_closure(1)
+    }
+
+    let answer = call_with_one_2(&|x| x + 2);
 
 }
--- closures/target/debug/closures.d
+++ closures/target/debug/closures.d
@@ -1,5 +1,5 @@
-C:\Users\Emma Ramirez\Code\rust\closures\target\debug\closures.d: src\main.rs
-
 C:\Users\Emma Ramirez\Code\rust\closures\target\debug\closures.exe: src\main.rs
+
+C:\Users\Emma Ramirez\Code\rust\closures\target\debug\closures.d: src\main.rs
 
 src\main.rs:
