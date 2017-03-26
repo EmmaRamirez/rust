387c1b3a46464be88cf6e3941b29f28cb193a947
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-25 19:46:21 -0600

    add section on use

---
+++ use_declaration/src/lib.rs
@@ -0,0 +1,29 @@
+#[allow(dead_code)]
+
+enum Status {
+    Rich,
+    Poor,
+}
+
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+fn main() {
+    use Status::{Poor, Rich};
+    use Work::*;
+
+    let status = Poor;
+    let work = Civilian;
+
+    match status {
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        Civilian => println!("Civilians work!"),
+        Soldier => println!("Soldiers fight!"),
+    }
+}
