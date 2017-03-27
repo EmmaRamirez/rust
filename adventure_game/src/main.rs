1bbb9df3e49dc291a30588cf3a528e5b1b432daf
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-27 03:44:02 -0600

    add cool bars

--- adventure_game/src/main.rs
+++ adventure_game/src/main.rs
@@ -1,3 +1,44 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::process::Command;
+
+
+
+struct Player {
+    hp: i32
+}
+
+impl Player {
+    fn hp(&self) -> &i32 { &self.hp }
+}
+
+fn create_screen(text: &str) -> String {
+    let mut output = String::new();
+    output.push_str("+-----------------------------------+\n");
+    output.push_str(text);
+    output.push_str("\n+-----------------------------------+");
+    output
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", create_screen("Welcome to Adventure Land!"));
+    // println!("
+    //         What class will you play as?
+    //         1. Warrior
+    //         2. Mage
+    //         3. Knight
+    // ");
+
+    // let mut playerClass = String::new();
+    //
+    // io::stdin().read_line(&mut playerClass)
+    //     .expect("failed to read line");
+    //
+    // let playerClass: u32 = match playerClass.trim().parse() {
+    //     Ok(num) => num,
+    //     Err(_) => continue,
+    // };
 }
