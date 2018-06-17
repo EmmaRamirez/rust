0ca92461551caad5f3597389ab34a919d2cd5b3a
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-06-17 16:47:12 -0600

    add web server

---
+++ web_server/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 152];
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
---
+++ web_server/target/debug/web_server.d
@@ -0,0 +1,1 @@
+/mnt/d/Code/rust/web_server/target/debug/web_server: /mnt/d/Code/rust/web_server/src/main.rs
